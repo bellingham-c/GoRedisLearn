818e717ead13ffb8813fc555e7d876fa025bf88b
cao <75364055@qq.com>
2023-05-18 23:20:56 +0800

    feat:encapsulate cache utils

--- controller/TestController.go
+++ controller/TestController.go
@@ -1,28 +1,13 @@
 package controller
 
 import (
-	"GoRedisLearn/RedisUtil"
+	"GoRedisLearn/model"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func Test(c *gin.Context) {
-	//var ctx = context.Background()
-	//rds := RedisUtil.GetClient()
-	//rds.Set(ctx, "test", nil, 30*time.Minute)
-	//
-	//res := rds.Get(ctx, "test").Val()
-	rds := RedisUtil.RedisUtil
-	err := rds.SETWithOutJson("test", nil, 30*time.Minute)
-	if err != nil {
-		panic(err)
-	}
-	res, err := rds.GET("test")
-
-	if res == "" {
-		fmt.Println("res", res)
-	}
-	fmt.Println("res123", res)
-
+	var t model.Test
+	c.ShouldBind(&t).Error()
+	fmt.Println("t", t)
 }
--- model/VoucherOrder.go
+++ model/VoucherOrder.go
@@ -4,13 +4,13 @@ import "time"
 
 type TbVoucherOrder struct {
 	Id         int
-	UserId     int
-	VoucherId  int
-	PayType    int
-	Status     int
-	CreateTime time.Time
-	UpdateTime time.Time
-	PayTime    time.Time
-	UseTime    time.Time
-	RefundTime time.Time
+	UserId     int       // 下单的用户id
+	VoucherId  int       // 购买的代金券id
+	PayType    int       // 支付方式
+	Status     int       // 订单状态
+	CreateTime time.Time // 下单时间
+	UpdateTime time.Time // 支付时间
+	PayTime    time.Time // 核销时间
+	UseTime    time.Time // 退款时间
+	RefundTime time.Time // 更新时间
 }
