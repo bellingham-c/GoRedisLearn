dec951e6b9299b21dcb71aaffcc3b473caf009dd
cao <75364055@qq.com>
2023-05-21 22:19:25 +0800

    feat:lua script

--- controller/TestController.go
+++ controller/TestController.go
@@ -3,12 +3,26 @@ package controller
 import (
 	"GoRedisLearn/RedisUtil"
 	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"time"
+	"os"
 )
 
 func Test(c *gin.Context) {
 	var ctx = context.Background()
 	rds := RedisUtil.GetClient()
-	rds.Set(ctx, "caojinbo", "test", 30*time.Second)
+	// Define Lua Script
+	script, err := os.ReadFile("./lua/a.lua")
+	fmt.Println("script", string(script))
+	// Call EVAL command
+	err = rds.Eval(ctx, string(script), []string{"capjinbo"}, "value").Err()
+	if err != nil {
+		// handle error
+		//panic(err)
+	} else {
+		fmt.Println("SET success")
+	}
+	res := rds.Get(ctx, "caojinbo").Val()
+	fmt.Println("res", res)
+
 }
